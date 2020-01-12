1cfc77e83c83016856925152f49c72e733abeb31
Matthew Reichardt <me@matthewreichardt.com>
2020-01-12 21:56:13 +0900

    Started account view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,20 +31,36 @@
                 box-shadow: 10px 11px 47px -15px rgba(0, 0, 0, 0.75);
             }
 
+            .outline {
+                border: 1px solid #37474F;
+                border-radius: 10px;
+                margin: 10px;
+            }
+
+            .account {
+                padding-left: 30px;
+            }
+
+            .account .panel {
+                padding: 20px 30px 20px 30px;
+                background: #fff;
+                color: #000;
+            }
+
             #start {
-                position: absolute;
+                position: fixed;
                 height: 100%;
                 padding: 30px;
                 overflow: auto;
             }
 
             #start .row {
-                
+                margin: 0px;
             }
 
-            #start img {
+            #start .brand {
                 width: 35%;
-                margin: 15% auto;
+                margin: 10% auto;
                 border-radius: 50%;
             }
 
@@ -54,8 +70,9 @@
                 color: #37474F;
                 bottom: 0px;
                 left: 0px;
-                height: 55%;
+                height: 65%;
                 border-radius: 0px 0px 10px 10px;
+                overflow: auto;
             }
 
             #start #content h2 {
@@ -75,7 +92,7 @@
 
             #start #content a {
                 width: 45%;
-                margin: 40px auto;
+                margin: 30px auto;
             }
 
             #start #content a i {
@@ -97,34 +114,101 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
+
+            .account .header {
+                width: 100%;
+                margin: 40px 0px 100px 0px;
+                background: #37474F;
+                border-radius: 10px;
+                color: #fff;
+            }
+
+            .account .header .avatar {
+                float: left;
+                width: 150px;
+                margin: -35px auto;
+                border: 10px solid #37474F;
+                border-radius: 50%;
+            }
+
+            .account .header .status {
+                float: left;
+                margin: 24px 30px 0px 20px;
+                font-size: 18pt;
+                font-weight: 600;
+            }
+
+            .account .header .status span {
+                font-size: 12pt;
+                font-weight: 400;
+            }
+
+            .account h4 {
+                margin: 0px 0px 5px 0px;
+            }
+
+            .account .card {
+                padding: 10px 15px 10px 15px;
+                border-radius: 10px;
+            }
         </style>
     </head>
     <body>
         <div class="container-fluid" id="start">
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-                <a href="{{ url('/logout') }}">Logout</a>
-            @else
-                <a href="{{ url('/login/facebook') }}">Login</a>
-            @endauth
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-4 panel">
-            <div class="row">
-            <img class="d-flex flex-wrap align-items-center" src="https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/81675168_132606801539585_556426993315348480_n.png?_nc_cat=108&_nc_oc=AQlKSBe5LXmbdMpiQ5Cssey5gVSbu88vuk8e8IAhxKC9YAViD_4_44mHd9QVjuCbZlo&_nc_ht=scontent-icn1-1.xx&oh=798cf55c414ce77d19272b189a662079&oe=5EA74D0C" />
-            </div>
-            <div class="row">
-            <div class="col-12" id="content">
-                <h2>Get verified</h2>
-                <hr />
-                <p class="lead">Start by logging in with your Facebook account. You will then be prompted for your Air Force email address.</p>
-                <p>A verification email will then be sent.</p>
-                <div class="row">
-                <a class="btn btn-lg btn-primary" href="#"><i class="fab fa-facebook"></i> Login</a>
+            <div class="row" style="height: 100%;">
+                <div class="col-sm-12 col-md-6 col-lg-4 panel">
+                    <div class="row">
+                        <img class="brand d-flex flex-wrap align-items-center" src="https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/81675168_132606801539585_556426993315348480_n.png?_nc_cat=108&_nc_oc=AQlKSBe5LXmbdMpiQ5Cssey5gVSbu88vuk8e8IAhxKC9YAViD_4_44mHd9QVjuCbZlo&_nc_ht=scontent-icn1-1.xx&oh=798cf55c414ce77d19272b189a662079&oe=5EA74D0C" />
+                    </div>
+                    <div class="row">
+                        <div class="col-12" id="content">
+                            <h2>Get verified</h2>
+                            <hr />
+                            <p class="lead">Start by logging in with your Facebook account. You will then be prompted for your Air Force email address.</p>
+                            <p>A verification email will then be sent.</p>
+                            <div class="row">
+                                @auth
+                                    <a class="btn btn-lg btn-secondary" href="{{ url('/logout') }}">Logout</a>
+                                @else
+                                    <a class="btn btn-lg btn-primary" href="{{ url('/login/facebook') }}"><i class="fab fa-facebook"></i> Login</a>
+                                @endauth
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                @auth
+                    <div class="col-sm-12 col-md-6 col-lg-8 account" style="padding-right: 0px;">
+                        <div class="col-sm-12 panel">
+                            <!-- Profile Header -->
+                            <div class="row">
+                                <div class="col-12 header">
+                                    <div class="status">Verified <span>2020-01-12</span></div>
+                                    <img class="avatar" src="https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/66025809_10156297112457327_7832456473002115072_n.jpg?_nc_cat=106&_nc_oc=AQmUSynEmT7B1vgTpoNQCJesrJ0UZc4wRiqYPnwPVbu8nV0xkkklCZLgT3w_JVxjzgU&_nc_ht=scontent-icn1-1.xx&oh=3073cdd2d4d290f801c40743757caaee&oe=5E9EDB43" />
+                                </div>
+                            </div>
+                            <!-- Confirm USAF Email Section -->
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h4><i class="fas fa-exclamation-triangle"></i> - USAF Affiliation Required</h4>
+                                    <p>Confirm your duty status below.</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 col-lg-6">
+                                    <div class="card">
+                                        d
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 col-lg-6">
+                                    <div class="card">
+                                        d
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endauth
             </div>
-            </div>
-        </div>
         </div>
 
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
