7d4d5378da21c0e21c0b640b95c02f4b6037e7e3
Matthew Reichardt <me@matthewreichardt.com>
2020-04-17 01:05:07 +0900

    Fixed email verify link generation

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -5,6 +5,6 @@
     <title>Verify - AFGaming.gg</title></head>
   <body>
     <p>Thank you for starting the Air Force Gaming verification process. Please click the link below to finalize your account creation.</p>
-    <a href="{{ url('/verify') }}?token={{ Auth::User()->usaf_verification }}">Verify!</a>
+    <a href="{{ url('/verify') }}/{{  Auth::User()->usaf_verification }}">Verify!</a>
   </body>
 </html>
